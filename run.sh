b8cfdf3de9949d1a662eb1e933a9a5b2df028683
moomoofarm1 <46774289+moomoofarm1@users.noreply.github.com>
2025-07-23 23:08:27 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,10 @@ apt update && apt upgrade && \
   bash scripts/00_bootstrap.sh && \
   source ~/.bashrc
   
-bash scripts/01_create_conda_env.sh
+bash scripts/01_create_conda_env.sh && bash scripts/02_git_init.sh
+chmod +x scripts/03_uv_sync.sh
+bash scripts/03_uv_sync.sh
+# uv run pyinstaller --name roberta_active --onefile src/__main__.py
 
 chmod +x scripts/*.sh
 
