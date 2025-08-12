593b9720bcd6ae22b8ab006e77207aa46ce25ec5
moomoofarm1 <zhuojun.gu.se@gmail.com>
2025-08-12 13:34:11 +0200

    A bugfix that does not require from register_model in server.py

--- .ipynb_checkpoints/server-checkpoint.py
+++ .ipynb_checkpoints/server-checkpoint.py
@@ -1,8 +1,22 @@
+# from fastapi import FastAPI
+# from label_studio_ml.api import register_model
+# from back.llm_backend import LLMInteractiveModel
+
+# app = FastAPI()
+
+# model = LLMInteractiveModel()
+# register_model(app, model)
+
 from fastapi import FastAPI
-from label_studio_ml.api import register_model
 from back.llm_backend import LLMInteractiveModel
 
 app = FastAPI()
-
 model = LLMInteractiveModel()
-register_model(app, model)
+
+@app.get("/health")
+def health():
+    return {"status": "UP"}
+
+@app.post("/predict")
+def predict(tasks: list):
+    return model.predict(tasks)
